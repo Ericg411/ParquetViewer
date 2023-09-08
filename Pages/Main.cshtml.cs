d82da432aed8fb63ed212f0991cd79377f0b5248
Ericg411 <eric.gauthier@zelis.com>
2023-09-08 14:06:34 -0500

    Finished API, began work on GUI

---
+++ Pages/Main.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ParquetViewerGUI.Pages
+{
+    public class MainModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
